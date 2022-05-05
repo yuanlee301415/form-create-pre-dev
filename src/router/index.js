dde9b3ef10205d2389c290eb6ebaaae15dfa762b
yuanlee <yuanlee@163.com>
2022-05-05 17:26:34 +0800

    forked from: [vue-template]@v0.3.0

---
+++ src/router/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Layout from '@/layout'
+
+Vue.use(Router)
+
+export const routes = [
+  { // Home
+    path: '/',
+    component: Layout,
+    redirect: '/',
+    children: [
+      {
+        path: '',
+        name: 'Home',
+        meta: {
+          title: 'Home'
+        },
+        component: () => import('@/views/home')
+      }
+    ]
+  }
+]
+
+export default new Router({
+  routes
+})
